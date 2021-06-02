4ea4c6a0a1644571adef3bf12653712229cdbaad
hinariEE <esugarmaple@gmail.com>
2021-06-02 18:52:05 +0800

    none

--- 13_3_BBCar_Calibration/main.cpp
+++ 13_3_BBCar_Calibration/main.cpp
@@ -2,16 +2,16 @@
 #include "bbcar.h"
 
 Ticker servo_ticker;
-PwmOut pin5(D5), pin6(D6);
+PwmOut pin5(D12), pin6(D13);
 
 BBCar car(pin5, pin6, servo_ticker);
 
 int main() {
    // please contruct you own calibration table with each servo
    double pwm_table0[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
-   double speed_table0[] = {-10.445, -9.812, -9.647, -9.408, -5.900, 0.000, 5.900, 10.843, 11.880, 11.401, 12.199};
+   double speed_table0[] = {-16.742, -16.104, -14.589, -10.523, -4.006, 0.558, 6.936, 12.436, 15.227, 16.343, 16.742};
    double pwm_table1[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
-   double speed_table1[] = {-10.445, -9.812, -9.647, -9.408, -5.900, 0.000, 5.900, 10.843, 11.880, 11.401, 12.199};
+   double speed_table1[] = {-16.423, -16.024, -15.067, -11.560, -5.262, 0.000, 6.856, 12.516, 15.227, 16.263, 16.662};
 
    // first and fourth argument : length of table
    car.setCalibTable(11, pwm_table0, speed_table0, 11, pwm_table1, speed_table1);
